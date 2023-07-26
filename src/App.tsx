e4d4edcb72c1458b154328a465e95c590f9d2223
Dickson Wu <wbs.dickson@gmail.com>
2023-07-26 17:28:05 -0600

    update home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import "./App.css";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 function App() {
     return (
-        <>
-            <div>
-                <h1>123</h1>
-            </div>
-        </>
+        <main className="app transition-all ease-in">
+            <Home />
+            <Canvas />
+            <Customizer />
+        </main>
     );
 }
 
