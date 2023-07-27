a9a048eec436856e2166c56904704f205aaae0d5
Dickson Wu <wbs.dickson@gmail.com>
2023-07-27 04:15:26 -0600

    add express server

---
+++ server/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import dalleRoutes from "./routes/dalle.routes";
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+app.listen(8080, () => console.log("Server has started on port 8080"));
