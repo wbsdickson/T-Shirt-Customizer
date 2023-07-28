347c6293cd099e0d82cc04a4500ff82db3a837ce
Dickson Wu <wbs.dickson@gmail.com>
2023-07-28 01:53:35 -0600

    update cors

--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,11 @@ import "dotenv/config";
 import dalleRoutes from "./routes/dalle.routes";
 
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(express.json({ limit: "50mb" }));
 
 app.use("/api/v1/dalle", dalleRoutes);
