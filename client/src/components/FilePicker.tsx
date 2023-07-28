0839d6101e4f21631ce16bce7d308a80798887ab
Dickson Wu <wbs.dickson@gmail.com>
2023-07-27 22:20:10 -0600

    add preview on upload

--- client/src/components/FilePicker.tsx
+++ client/src/components/FilePicker.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import { useState } from "react";
 
 import CustomButton from "./CustomButton";
 
 const FilePicker = ({ file, setFile, readFile }) => {
+    const [previewImage, setPreviewImage] = useState(null);
+
     return (
         <div className="filepicker-container">
             <div className="flex-1 flex flex-col">
-                <input id="file-upload" type="file" accept="image/*" onChange={(e) => setFile(e.target.files[0])} />
+                <input
+                    id="file-upload"
+                    type="file"
+                    accept="image/*"
+                    onChange={(e) => {
+                        const file = e.target.files[0];
+                        setFile(file);
+                        const reader = new FileReader();
+                        reader.onloadend = () => {
+                            setPreviewImage(reader.result);
+                        };
+                        reader.readAsDataURL(file);
+                    }}
+                />
                 <label htmlFor="file-upload" className="filepicker-label">
-                    Upload File
+                    Upload Image
                 </label>
 
                 <p className="mt-2 text-gray-500 text-xs truncate">{file === "" ? "No file selected" : file.name}</p>
+                {previewImage && (
+                    <img src={previewImage} alt="Preview" style={{ width: "300px", height: "auto", marginTop: "10px" }} />
+                )}
             </div>
 
             <div className="mt-4 flex flex-wrap gap-3">
